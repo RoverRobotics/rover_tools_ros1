5d9398e426fde32607e9706694ddc1e5f27319a8
jbrover <john@roverrobotics.com>
2021-05-12 14:37:51 -0500

    add json framework for variable manipulation of bash commands

--- install/install.py
+++ install/install.py
@@ -4,6 +4,15 @@ import os
 with open("shellscripts.json", "r") as inputfile:
     data = json.load(inputfile)
 
+with open("variables.json", "r") as inputfile:
+    variables = json.load(inputfile)
+
+def replace_matched_variables(command):
+    for var in variables:
+        if var in command:
+            command = command.replace(var, variables[var])
+
+    return command
 
 for key in data:
     for command_set in data[key]:
@@ -12,9 +21,11 @@ for key in data:
             
             if isinstance(commands, list):
                 for command in commands:
+                    command = replace_matched_variables(command)
                     print(command)
             else:
-                print(commands)
+                command = replace_matched_variables(commands)
+                print(command)
 
             print('SUCCESS!')
             print('')
