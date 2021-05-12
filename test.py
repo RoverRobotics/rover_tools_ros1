11684d2613ff785e0eb5ed63bae5ccf1ce5e2dc3
drhieu <will@roverrobotics.com>
2021-05-12 13:56:25 -0500

    added shellscript json

--- test.py
+++ test.py
@@ -1,20 +1,25 @@
 import pexpect
 import sys
 import boto3
-import os
-def check():
-    global errorno
-    if (errorno != 0):
-        print ("Error")
+import json
 
-errorno = os.system("sudo apt-get update")
-check()
-errorno = os.system("sudo usermod -aG dialout,sudo,input $USER")
-check()
-errorno = os.system(''' sudo sh -c 'echo "deb http://packages.ros.org/ros/ubuntu $ (lsb_release -sc) main" > /etc/apt/sources.list.d/ros-latest.list' ''')
-check()
-errorno = os.system(''' sudo apt-key adv --keyserver 'hkp://keyserver.ubuntu.com:80' --recv-key C1CF6E31E6BADE8868B172B4F42ED6FBAB17C654 ''')
-check()
+import os
+
+with open(r'./shellscripts.yaml') as file:
+
+# scripts = yaml.load("./shellscripts.yaml")
+print(yaml.dump(yaml.load(document), default_flow_style=False))
+
+//subprocess (shell = true,  "source setup.sh ; env>tmpfile.txt") 
+
+# errorno = os.system("sudo apt-get update")
+# check()
+# errorno = os.system("sudo usermod -aG dialout,sudo,input $USER")
+# check()
+# errorno = os.system(''' sudo sh -c 'echo "deb http://packages.ros.org/ros/ubuntu $ (lsb_release -sc) main" > /etc/apt/sources.list.d/ros-latest.list' ''')
+# check()
+# errorno = os.system(''' sudo apt-key adv --keyserver 'hkp://keyserver.ubuntu.com:80' --recv-key C1CF6E31E6BADE8868B172B4F42ED6FBAB17C654 ''')
+# check()
 # def start_log():
 #     global child
 #     child.logfile = open("./mylog", "w")
