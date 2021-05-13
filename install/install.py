3aaf086d33014f1dcc2d021ad0db9764049a387c
jbrover <john@roverrobotics.com>
2021-05-13 15:24:55 -0500

    install works

--- install/install.py
+++ install/install.py
@@ -1,6 +1,6 @@
 from abc import get_cache_token
 import json
-import os
+import os, stat
 import subprocess
 import pexpect
 import sys
@@ -34,57 +34,33 @@ class RobotPackageInstaller():
 
     def run_install(self, logfile_location="test.log"):
         password = input("Please enter your password: ")
-        print ("finished waiting")
-        robotscript = pexpect.spawn("/bin/bash" ,encoding='UTF-8',echo = True)
+        print ("starting install....")
+
+        # open a master install log
         fout = open(logfile_location,'wb')
-        # robotscript.logfile = fout
-        robotscript.logfile = sys.stdout
-        # robotscript.logfile_read = sys.stdout
-        robotscript.sendline("sudo -s")
-        robotscript.expect("password", timeout=2)
-        robotscript.sendline(password)
+        fout.close()
+
         for play in self.playbooks[self.model]:
             for command_set in self.install_commands[play]:
                 for command_category, commands in command_set.items():
-                    # print('DOING %s PART OF INSTALLATION' %
-                    #         command_category)
-
+                    
                     if isinstance(commands, list):
-                        for command in commands:
-                            command = self.replace_matched_variables(
-                                command)
-                            # print(command)
-                            try:
-                                # print("attempting command %s" % command)
-                                robotscript.sendline(command)
-                                # robotscript.sendline(command.encode(encoding='UTF-8'))
-                                # print (robotscript.before + robotscript.after)
-                                # robotscript.expect(">")
-                            except (SystemError, FileNotFoundError):
-                                print ("Failed to run this bash command: %s" % command);
-
+                        with open("temp.sh", "w") as temp_bash:
+                            for command in commands:
+                                command = self.replace_matched_variables(command)
+                                temp_bash.write(command+'\n')  
                     else:
-                        command = self.replace_matched_variables(
-                            commands)
-                        # print(command)
+                        with open("temp.sh", "w") as temp_bash:
+                            command = self.replace_matched_variables(commands)
+                            temp_bash.write(command+'\n')
+
+                    print(os.getcwd())
+                    fout = open(logfile_location,'a')
+                    os.chmod("temp.sh", stat.S_IROTH | stat.S_IWOTH | stat.S_IXOTH | stat.S_IRUSR | stat.S_IWUSR |  stat.S_IXUSR)
+                    os.system("echo %s | %s" % (password, "/bin/bash temp.sh"))
 
 
 if __name__ == "__main__":
     install = RobotPackageInstaller()
     install.set_model('test')
     install.run_install()
-# for key in data:
-#     for command_set in data[key]:
-#         for command_category, commands in command_set.items():
-#             print('DOING %s PART OF INSTALLATION' % command_category)
-
-#             if isinstance(commands, list):
-#                 for command in commands:
-#                     command = replace_matched_variables(command)
-#                     print(command)
-#             else:
-#                 command = replace_matched_variables(commands)
-#                 print(command)
-
-#             print('SUCCESS!')
-#             print('')
