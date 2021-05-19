15c573242e048f531a5510129175c0a5531acce6
jbrover <john@roverrobotics.com>
2021-05-19 16:04:45 -0500

    bug fixes

--- start.sh
+++ start.sh
@@ -3,6 +3,7 @@ sudo apt-get update
 sudo apt-get install python3-pip -y
 pip3 install console-menu
 pip3 install boto3
+pip3 install pexpect
 python3 menu/menu_main.py
 source /opt/ros/melodic/setup.bash
 source ~/catkin_ws/devel/setup.bash
