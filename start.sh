d4f153aafdeccd784d046bf18ce1c0a8baf34ebf
Nick Fragale <nicholas.fragale@gmail.com>
2021-08-07 12:39:19 -0500

    Revert "Update and rename start.sh to install.sh"
    This reverts commit cdab8d0d4d878c6ecf3faca8c5b808af3924d07f.

--- start.sh
+++ start.sh
@@ -8,4 +8,8 @@ pip3 install boto3
 pip3 install pexpect
 cp -rf ../rover_tools ~/
 git clone https://github.com/roverrobotics/robottests ~/rover_tools/testing/robottests/
-
+cwd=$(pwd)
+cf="${cwd}/../credentials.json"
+python3 ~/rover_tools/menu/menu_main.py --cf ${cf}
+source /opt/ros/melodic/setup.bash
+source ~/catkin_ws/devel/setup.bash
